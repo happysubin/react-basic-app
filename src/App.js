88908e32f72c76ccfa95038985836b4526238beb
happysubin <76802855+happysubin@users.noreply.github.com>
2021-07-03 17:55:41 +0900

    class component basic 1

--- src/App.js
+++ src/App.js
@@ -1,86 +1,27 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-function Ex1() {
-  return <h2>hi everyOne!</h2>;
-}
-function Food({ image, name, rating }) {
-  return (
-    <div>
-      <h3>I LOVE {name}!!!!</h3>
-      <h4> {rating}/5.0 </h4>
-      <img src={image} alt="no img" />
-    </div>
-  );
-}
+//component의 데이터를 바꾸기
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 4.9,
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 5,
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.4,
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 4.9,
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.8,
-  },
-];
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
-
-function App() {
-  return (
-    <div>
-      <h1>Hello Wolrd!!!!</h1>
-      <Ex1></Ex1>
-      {foodILike.map((food) => {
-        return (
-          <Food
-            key={food.id}
-            name={food.name}
-            image={food.image}
-            rating={food.rating}
-          ></Food>
-        );
-      })}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render() {
+    return (
+      <div>
+        <h1>HELLO WORLD {this.state.count}</h1>
+        <button onClick={this.add}>ADD</button>
+        <button onClick={this.minus}>MINUS</button>
+      </div>
+    );
+  }
 }
-/* 
-function renderFood(dish){
-  return <Food name={dish.name} image={dish.image}/>; 이 함수를 추가
-}
-return <div> {foodILike.map(renderFood)} </div>; 53~ 55번을 이것으로 변경
-이것도 위의 로직과 같다
-*/
+//this.add()가 아닌 이유는 click시에만 함수를 호출하기 때문이다. this.add()면 항시 호출이 되어서 이렇게 코드를 작성하지 않는다.
+
 export default App;
