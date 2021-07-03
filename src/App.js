a96caa0ab96dec4f09902ae394d9ddb0a979a141
happysubin <76802855+happysubin@users.noreply.github.com>
2021-07-03 15:18:19 +0900

    react basic 3 (map ver 2)

--- src/App.js
+++ src/App.js
@@ -7,33 +7,38 @@ function Food({ image, name }) {
   return (
     <div>
       <h3>I LOVE {name}!!!!</h3>
-      <img src={image} />
+      <img src={image} alt="no img" />
     </div>
   );
 }
 
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
   },
   {
+    id: 2,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
   },
   {
+    id: 3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
   },
   {
+    id: 4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
   },
   {
+    id: 5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
@@ -46,10 +51,16 @@ function App() {
       <h1>Hello Wolrd!!!!</h1>
       <Ex1></Ex1>
       {foodILike.map((food) => {
-        return <Food name={food.name} image={food.image}></Food>;
+        return <Food key={food.id} name={food.name} image={food.image}></Food>;
       })}
     </div>
   );
 }
-
+/* 
+function renderFood(dish){
+  return <Food name={dish.name} image={dish.image}/>; 이 함수를 추가
+}
+return <div> {foodILike.map(renderFood)} </div>; 53~ 55번을 이것으로 변경
+이것도 위의 로직과 같다
+*/
 export default App;
