53102f7670a11b5f1cd7855dc629272a9b86a251
happysubin <76802855+happysubin@users.noreply.github.com>
2021-07-03 19:18:14 +0900

    before start movie app

--- src/App.js
+++ src/App.js
@@ -4,48 +4,21 @@ import React from "react";
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
-    console.log("born");
+    super(props); //super 키워드는 부모 오브젝트의 함수를 호출할 때 사용됩니다.
+    this.state = {
+      count: 0,
+      isLoading: true,
+      movies: [],
+    };
   }
-  state = {
-    count: 0,
-  };
-  add = () => {
-    //this.state.count = +1;  state를 바로 직접적으로 바꿀 수 없다.
-    //this.setState({ count: this.state.count + 1 }); 이건 좋은코드는 아니다.
-    this.setState((current) => ({
-      //화살표함수의 this 는 상위 객체를 가리킨다.
-      //만약 js 일반 function 이면 this 가 자신을 가리켜서 상위 객체를 가리키기 위해 화살표 함수를 사용한다.
-      //setState는 비동기이다. 다른곳에서도 count를 호출하면 계산이 이상해질 수 있기 때문이다.
-      count: current.count + 1,
-    }));
-  };
-  minus = () => {
-    //current 는 현재의 state를 가리킨다.
-    this.setState((current) => ({
-      count: current.count - 1,
-    }));
-  };
-
   componentDidMount() {
-    console.log("Component rendered");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-  componentWillUnmount() {
-    console.log("Goodbye, cruel world");
-  }
-
   render() {
-    console.log("render!!!!");
-    return (
-      <div>
-        <h1>HELLO WORLD {this.state.count}</h1>
-        <button onClick={this.add}>ADD</button>
-        <button onClick={this.minus}>MINUS</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "we are loading.." : "we are ready"}</div>;
   }
 }
 //this.add()가 아닌 이유는 click시에만 함수를 호출하기 때문이다. this.add()면 항시 호출이 되어서 이렇게 코드를 작성하지 않는다.
