cc3a6664804e8d88b349890b09fd13eca474ea2e
happysubin <76802855+happysubin@users.noreply.github.com>
2021-07-03 14:35:37 +0900

    react basic 2 (jsx, component)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
-import Ex1 from "./ex1";
+
+function Ex1() {
+  return <h2>hi everyOne!</h2>;
+}
+function Food({ fav, name }) {
+  //obj안에 fav라는 프로퍼티가 있다.
+  return (
+    <h3>
+      I LOVE {fav}!!!! {name}
+    </h3>
+  );
+}
 
 function App() {
   return (
     <div>
       <h1>Hello Wolrd!!!!</h1>
-      <Ex1 />
+      <Ex1></Ex1>
+      <Food fav="kimchi" name="first"></Food>
+      <Food fav="chicken"></Food>
+      <Food fav="ice-cream"></Food>
+      <Food fav="kimbap"></Food>
     </div>
   );
 }
--- src/ex1.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Example() {
-  return <h2>i love potato</h2>;
-}
-
-export default Example;
