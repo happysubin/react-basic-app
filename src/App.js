9e6677a15dc1722e7a7a89619d0921ccedf3f86f
happysubin <76802855+happysubin@users.noreply.github.com>
2021-07-03 15:43:07 +0900

    proptypes package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Ex1() {
   return <h2>hi everyOne!</h2>;
 }
-function Food({ image, name }) {
+function Food({ image, name, rating }) {
   return (
     <div>
       <h3>I LOVE {name}!!!!</h3>
+      <h4> {rating}/5.0 </h4>
       <img src={image} alt="no img" />
     </div>
   );
@@ -18,32 +20,43 @@ const foodILike = [
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 4.9,
   },
   {
     id: 2,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 5,
   },
   {
     id: 3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 4.4,
   },
   {
     id: 4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 4.9,
   },
   {
     id: 5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4.8,
   },
 ];
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 function App() {
   return (
@@ -51,7 +64,14 @@ function App() {
       <h1>Hello Wolrd!!!!</h1>
       <Ex1></Ex1>
       {foodILike.map((food) => {
-        return <Food key={food.id} name={food.name} image={food.image}></Food>;
+        return (
+          <Food
+            key={food.id}
+            name={food.name}
+            image={food.image}
+            rating={food.rating}
+          ></Food>
+        );
       })}
     </div>
   );
